e699bd869b7c8c7226b459bf69b8def2c008e786
slipstream8125 <hatkardhruv@gmail.com>
2023-10-02 16:37:20 +0530

    Added more stuff

--- Cyber-0-Day/cyber-0-day-script.sh
+++ Cyber-0-Day/cyber-0-day-script.sh
@@ -1,32 +1,34 @@
 cd #cd into the home directory
-git clone https://github.com/Fausto-Korpsvart/Gruvbox-GTK-Theme.git #Getting the Gruvbox-GTK theme
+git clone https://github.com/Fausto-Korpsvart/Nightfox-GTK-Theme.git #Getting the Gruvbox-GTK theme
 mkdir ~/.themes #making a directory for themes
-cp -r Gruvbox-GTK-Theme/themes/Gruvbox-Dark-B .themes/Gruvbox-Dark-B # Transferring the required theme to the themes folder
-cp -r .themes/Gruvbox-Dark-B /usr/share/themes/
-gsettings set org.gnome.desktop.interface gtk-theme "Gruvbox-Dark-B" # Changing the gtk theme
-gsettings set org.gnome.desktop.wm.preferences theme "Gruvbox-Dark-B"
+cp -r Nightfox-GTK-Theme/themes/Nightfox-Dusk-B .themes/ # Transferring the required theme to the themes folder
+cp -r .themes/Nightfox-Dusk-B /usr/share/themes/
+gsettings set org.gnome.desktop.interface gtk-theme "Nightfox-Dusk-B" # Changing the gtk theme
+gsettings set org.gnome.desktop.wm.preferences theme "Nightfox-Dusk-B"
 echo "Removing Unnecessary Git repos"
-rm -r Gruvbox-GTK-Theme
+rm -r Nightfox-Dusk-B
 gnome-shell-extension-tool -e user-theme@gnome-shell-extensions.gcampax.github.com
-gsettings set org.gnome.shell.extensions.user-theme name "Gruvbox-Dark-B"
+gsettings set org.gnome.shell.extensions.user-theme name "Nightfox-Dusk-B"
 git clone https://github.com/SylEleuth/gruvbox-plus-icon-pack.git
 mkdir ~/.icons
 cp -rv gruvbox-plus-icon-pack ~/.icons/GruvboxPlus
 gsettings set org.gnome.desktop.interface icon-theme 'GruvboxPlus'
 rm -r gruvbox-plus-icon-pack
 gsettings set org.gnome.desktop.background picture-uri "cyber-0-day.png"
-curl -fsSL https://raw.githubusercontent.com/manilarome/blurredfox/script/install.sh | bash -s -- esr
-git clone --depth=1 --single-branch https://github.com/realmazharhussain/gdm-tools.git
 cd gdm-tools
 ./install.sh 
 cd ..
 rm -r gdm-tools
-set-gdm-theme -s Gruvbox-Dark-B
-sudo apt install neofetch vim neovim
+set-gdm-theme -s Nightfox-Dusk-B
+# Removing Defaults
+sudo rm -r /usr/share/images/vendor-logos
+sudo rm -r /usr/share/plymouth/plymouthd.defaults
+#Installing more stuff
+sudo apt install neofetch vim neovim docker-ce-cli
 pip install --upgrade gnome-extensions-cli
 gext install widgets@Aylur
 sudo apt -y install curl && bash <(curl -Ls https://github.com/Tux4Ubuntu/tux-install/raw/master/install.sh)
-git clone https://git.fs.lmu.de/adnan/grub2-gruvbox.git
-cd grub2-gruvbox/
+git clone https://github.com/slipstream8125/LUGOS-grub.git
+cd LUGOS-grub/LUGOS-grub
 echo "Enter Password for sudo access"
 sudo ./install.sh
